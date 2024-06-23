66d748ce6282a0064e2b2e6065bd07c3d874de6a
GianBeltran <Gbeltranl0506@gmail.com>
2024-06-22 20:53:37 -0500

    Avance de pregunta 2

--- src/app/principal/estacionamiento/estacionamiento.component.ts
+++ src/app/principal/estacionamiento/estacionamiento.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-estacionamiento',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, CommonModule],
   templateUrl: './estacionamiento.component.html',
   styleUrl: './estacionamiento.component.css'
 })
 export class EstacionamientoComponent {
+
+  horas: number = 0;
+  minutos: number = 0;
+  costo: number = 0;
+  tarifaPorHora: number = 1.500; // Tarifa por hora en soles
+
+  calcularCosto(): void {
+    const tiempoTotalEnHoras = this.horas + (this.minutos / 60);
+    this.costo = Math.ceil(tiempoTotalEnHoras) * this.tarifaPorHora;
+  }
 
 }
