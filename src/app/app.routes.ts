e759c5ae793d61833955f8008bf69fda227c3eeb
GianBeltran <Gbeltranl0506@gmail.com>
2024-06-22 20:41:15 -0500

    Final pregunta 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { authGuard } from './auth.guard';
+import { EstacionamientoComponent } from './principal/estacionamiento/estacionamiento.component';
+import { AlumnoComponent } from './principal/alumno/alumno.component';
+import { AlmacenComponent } from './principal/almacen/almacen.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './principal/home/home.component';
 
 export const routes: Routes = [
 
-    {path: "login", component: LoginComponent}
+    {path: "login", component: LoginComponent},
+    {path: "principal", component: PrincipalComponent,
+        canActivate: [authGuard],
+        children: [
+            {path: "home", component: HomeComponent},
+            {path: "estacionamiento", component: EstacionamientoComponent},
+            {path: "alumno", component: AlumnoComponent},
+            {path: "almacen", component: AlmacenComponent}
+        ]
+    },
+
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path: "**", component: PageNotFoundComponent}
 
 ];
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../angular-material/material/material.module';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  constructor(private router:Router, private route:ActivatedRoute, private authService: AuthService){
+    
+  }
+
+  irHome(): void{
+    this.router.navigate(["home"], {relativeTo: this.route})
+  }
+  irEstacionamiento(): void{
+    this.router.navigate(["estacionamiento"], {relativeTo: this.route})
+  }
+  irAlumno(): void{
+    this.router.navigate(["alumno"], {relativeTo: this.route})
+  }
+  irAlmacen(): void{
+    this.router.navigate(["almacen"], {relativeTo: this.route})
+  }
+
+  salir(): void{
+    this.authService.logout();
+    this.router.navigateByUrl("login");
+  }
 
 }
